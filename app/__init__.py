602b04d9624698f39c7e43899207c182e30c99c1
Ming <ming860624@yahoo.com.tw>
2024-07-12 22:17:43 +0800

    Homepage display completed, CSS added, user profile settings

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Integer, String, URL
-from sqlalchemy.orm import Mapped, mapped_column
-from typing import Optional
 
 from dotenv import load_dotenv
 import os
@@ -11,11 +8,12 @@ import os
 
 class Base(DeclarativeBase):
     pass    
-db = SQLAlchemy(model_class=Base)
+db = SQLAlchemy(model_class=Base)  #DB
 
-def create_app():
-    """Create and configure an instance of the Flask application."""
-     app = Flask(__name__)
+
+def create_app(test_config=None):
+    """建立並配置一個 Flask 應用程式的實體"""
+    app = Flask(__name__)
     
     load_dotenv()  #從 .env 檔中載入環境變數道系統中。或者也可透過 set 指令直接設定變數值。
     db_url = os.environ.get("SQLALCHEMY_DATABASE_URI")  #從系統(shell)中找環境變數，可透過 set 指令設定變數值。
@@ -23,23 +21,37 @@ def create_app():
     if db_url is None: 
         db_url = "sqlite:///flaskr.sqlite"
 
+    #應用載入配置
     app.config.from_mapping(
         # SECRET_KEY=os.environ.get("SECRET_KEY", "dev"),
         SQLALCHEMY_DATABASE_URI=db_url,
     )  
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.update(test_config)
 
     db.init_app(app)
 
+    #套用藍圖到應用中
+    from .views import home_bp
+
+    app.register_blueprint(home_bp)
     app.add_url_rule("/", endpoint="index")
 
     return app
 
+#初始化資料庫
+def init_db():
+    db.drop_all()
+    db.create_all()
 
-# class User(db.Model):  #也可以直接繼承 Base
-#     __tablename__ = "user_account"  #不一定要有，如果沒有指定名稱則會直接使用類別名稱
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     username: Mapped[str] = mapped_column(String(30), unique=True)
-#     # email: Mapped[Optional[str]]  #使用 Optional[] 讓欄位可以是 null。 目前無法使用，可能是bug
-    
-# with app.app_context():
-#     db.create_all()
+#定義 CLI 指令－初始化資料料庫
+@click.command("init-db")
+@with_appcontext
+def init_db_command():
+    """Clear existing data and create new tables."""
+    init_db()
+    click.echo("Initialized the database.")
---
+++ app/views/__init__.py
@@ -0,0 +1,2 @@
+from.home import bp as home_bp
+from.auth import bp as auth_bp
