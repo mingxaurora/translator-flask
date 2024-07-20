0f846e906d856a0ccade95200800017b96e448f7
Ming <ming860624@yahoo.com.tw>
2024-07-20 22:22:28 +0800

    Completed History block. Completed translation function.

--- app/views/home.py
+++ app/views/home.py
@@ -2,32 +2,39 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request,session ,url_for
 )
 
-from app.until.translation import translate_func
+from app.utils.translation import translate_func
 
 from app import db
 from app.models.history_model import History
-from app.views.auth import login_required
+# from app.views.auth import login_required
 
 bp = Blueprint('home', __name__)
 
 #首頁
 @bp.route('/')
 def index():
+
+    #從參數中取得譯文
     translated_text = request.args.get('translated_text')
+    #取得 historys，會顯示在頁面中
+    if session.get("user_id"):
+        select = db.select(History) \
+            .where(History.owner_id == session["user_id"]) \
+            .order_by(History.created_at.desc())
+        historys = db.session.execute(select).scalars()
+        # 也能使用 historys.all() 轉成 list。根據使用上的需求。
+    else:
+        historys = None
+    
     datas = {
-        'translated_text': translated_text
+        'translated_text': translated_text,
+        'historys': historys
     }
+
     return render_template('home/index.html', **datas)
 
-# @bp.route('/test_page')
-# def test_page():
-#     translated_text = request.args.get('translated_text')
-#     datas = {
-#         'translated_text': translated_text
-#     }
-#     return render_template('test.html', **datas) 
 
-@bp.route('/test', methods=['POST'])
+@bp.route('/translate', methods=['POST'])
 def translate():
     original_text = request.form['original_text']
     error = None
@@ -49,15 +56,3 @@ def translate():
         return redirect(url_for('home.index', translated_text=translated_text))
 
     return render_template('home/index.html')
-
-
-
-
-#測試
-# @login_required
-# @bp.route('/test', methods=("GET", "POST"))
-# def index():
-#     str1 = 'test'
-#     print(str1)
-#     return(str1)
-
